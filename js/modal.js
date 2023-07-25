50d05fe4b82c9d35f05b7649b2b2d76434adc637
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-07-25 17:20:07 +0300

    Update

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]",),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
@@ -10,5 +10,18 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("modal-open")
   }
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && !refs.modal.classList.contains("is-hidden")) {
+      toggleModal(false);
+    }
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === refs.modal) {
+      toggleModal(false);
+    }
+  });
 })();
